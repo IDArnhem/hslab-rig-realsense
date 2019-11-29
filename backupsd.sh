46472bb5027e626545b3380c0c9b4621629f26e4
zilog <dropmeaword@gmail.com>
2019-11-29 19:19:29 +0100

    updated script with additional check

--- backupsd.sh
+++ backupsd.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo dd if=/dev/mmcblk0p1 bs=4M | gzip > jetsonnano@rig__sd-card-backup.img.gz
+DEVICE_PATH=/dev/mmcblk0p1
+
+if [ ! -d $DEVICE_PATH || -z "$1" ] then
+    echo "Don't know which device to back-up in $DEVICE_PATH, insert card or tell me which device to back-up"
+else
+    DEVICE_PATH = $1
+    sudo dd if=$DEVICE_PATH bs=4M | gzip > jetsonnano@rig__sd-card-backup.img.gz
+fi

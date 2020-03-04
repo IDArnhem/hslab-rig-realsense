24ad1ef24410052be75d11ac6be2a82e30b42e7b
zilog <dropmeaword@gmail.com>
2020-03-04 20:22:53 +0100

    added sanity check script

---
+++ sanitycheck.sh
@@ -0,0 +1,34 @@
+## checks if the realsense and NDI libraries are installed in the current system
+
+missing() {
+	echo "\e[5m\e[31m$1 is missing, please install it.\e[39m\e[25m"
+}
+
+present() {
+	#echo "\xE2\x9C\x94"
+	echo "\e[32m $1 is ok\e[39m"
+}
+
+LIBDIRS="/usr/local/lib /usr/lib /usr/lib/x86_64-linux-gnu"
+CHECK="librealsense2.so libNDI.so"
+for fname in $CHECK; do
+  found=false
+  #echo "Checking at $dir..."
+  for dir in $LIBDIRS; do
+    echo "Checking $dir/$fname"
+    if [ -f "$dir/$fname" ]; then
+    	#present "$fname is there!"
+	found=true
+    else
+	#missing "$fname is not there!"
+	found=false
+    fi
+  done
+
+  if [ "$found" = true ] ; then
+     present "$fname"
+  else
+     missing "$fname"
+  fi
+
+done
